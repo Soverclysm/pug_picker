fd1651a68a3cce14d1c36cf2fb57ce7bec843a36
CatraMyBeloved <ole.stein@alumni.fh-aachen.de>
2024-12-24 18:28:29 +0100

    basic single player visualization

--- src/analysis/pug_analysis.py
+++ src/analysis/pug_analysis.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.figure import figaspect
+
 
 class PUGAnalysis:
 
@@ -9,22 +11,21 @@ class PUGAnalysis:
         self.data = pd.read_csv(csv_file)
 
     def _select_players(self, nicknames):
-        nicknames = [nicknames]
-        print(nicknames)
+        nicknames = [nicknames] if isinstance(nicknames, str) else list(nicknames)
         return self.data[self.data['nickname'].isin(nicknames)]
 
     def create_role_distribution(self, nickname):
         player_data = self._select_players(nickname)
+        fig, ax = plt.subplots()
         role_distribution = player_data['role'].value_counts()
         # Create pie plot
-        plt.figure(figsize=(8, 8))
-        plt.pie(role_distribution.values,
+        ax.pie(role_distribution.values,
                 labels=role_distribution.index,
                 autopct='%1.1f%%',
                 colors=sns.color_palette('pastel'))
-        plt.title('Roles distribution')
-        plt.axis('equal')
-        plt.show()
+        ax.title('Roles distribution')
+        ax.axis('equal')
+        return fig
 
     def descriptive_statistics(self, nickname):
         player_data = self._select_players(nickname)
@@ -35,8 +36,27 @@ class PUGAnalysis:
         role_distribution = player_data['role'].value_counts()
         role_winrates = player_data.groupby('role')['result'].mean()
 
-        games_as_captain = len(player_data[player_data['captain'] == True])
-        captain_winrate = player_data[player_data['captain'] == True &
-                                      player_data['result'] == 1].mean()
+        captain_games = player_data[player_data['captain'] == True]
+        captain_games_amount = len(captain_games)
+        captain_winrate = captain_games['result'].mean()
 
+        player_stats = {
+            'total_games': total_games,
+            'wins': wins,
+            'winrate': winrate,
+            'role_distribution': role_distribution,
+            'captain_games_amount': captain_games_amount,
+            'captain_winrate': captain_winrate,
+            'role_winrates': role_winrates
+        }
+        return player_stats
+
+
+    def plot_winrates(self, nickname):
+        player_data = self._select_players(nickname)
+        fig, ax = plt.subplots()
+        sns.barplot(x='role', y='result', data=player_data, estimator=np.mean, ax=ax)
+        plt.xlabel('Role')
+        plt.ylabel('Result')
+        return fig
 
